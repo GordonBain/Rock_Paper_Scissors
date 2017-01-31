63674625a5956927f3a670e073d5382c1bd709a9
Gordon Bain <gbain8893@gmail.com>
2017-01-31 08:42:06 +0000

    further commit

--- controller.rb
+++ controller.rb
@@ -15,3 +15,12 @@ get '/:choice1/:choice2' do
   @outcome = game1.scissors_wins 
   erb( :result )
 end
+
+
+# get '/:choice1/:choice2' do 
+#   p1 = params[:choice1]
+#   p2 = params[:choice2]
+#   game2 = Game.new(p1, p2)
+#   @outcome = game1.rock_wins 
+#   erb( :result )
+# end
